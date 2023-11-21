2c6e3b197b86a25aaf557fcf759d7505bb433b1f
Roab <roabsouza144@gmail.com>
2023-11-20 22:40:30 -0300

    department class implementation

---
+++ src/app/main.java
@@ -0,0 +1,16 @@
+package app;
+
+import model.entites.department;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		department dp = new department(1, "book");
+		
+		System.out.println(dp);
+		
+		
+	}
+
+}
